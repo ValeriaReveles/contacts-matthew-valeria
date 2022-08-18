77c23a10acdf2ec23112dfafebe4cdbf279d3768
Matthew Grayson <matthew.alan.grayson@gmail.com>
2022-08-18 15:20:12 -0500

    completed structure for contact class with getters and string methods

--- src/contactsmanager/Contact.java
+++ src/contactsmanager/Contact.java
@@ -1,7 +1,28 @@
 package contactsmanager;
 
 public class Contact {
-    public static void main(String[] args) {
+    private String name;
+    private Long phoneNumber;
 
+    public Contact(String name, Long phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + phoneNumber;
+    }
+    public static Contact fromString(String contactString) {
+        String[] parts = contactString.split(" | ");
+        return new Contact(parts[0], Long.parseLong(parts[1]));
     }
 }
